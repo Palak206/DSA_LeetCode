633741ee84f89a91c543f80957e61208db81fdfd
Palak206 <94644931+Palak206@users.noreply.github.com>
2022-11-27 12:44:40 +0530

    Added solution - LeetHub

---
+++ Linked List in Zig-Zag fashion - GFG/linked-list-in-zig-zag-fashion.cpp
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void print(Node *root)
+{
+	Node *temp = root;
+	while(temp!=NULL)
+	{
+		cout<<temp->data<<" ";
+		temp=temp->next;
+	}
+}
+ 
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+
+class Solution
+{
+    public:
+    Node *zigZag(Node* head)
+    {
+        if(head == NULL && head->next == NULL)
+          return head;
+          
+          
+        Node* current = head;
+        bool flag = true;
+        
+        
+        while(current->next != NULL){
+            if(flag){
+               if (current->data > current->next->data){
+                swap(current->data, current->next->data);
+               }
+               flag = !flag;
+            }
+        
+              else
+               {
+            
+                  if (current->data < current->next->data){
+                   swap(current->data, current->next->data);
+                  }
+              
+                 flag = !flag;
+               }
+             current = current->next;
+ 
+    }
+    return head;
+     // your code goes here
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		Solution ob;
+		Node *ans = ob.zigZag(head);
+		print(ans);
+		cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
