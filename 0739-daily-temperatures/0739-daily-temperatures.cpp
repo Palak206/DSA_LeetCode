f1d4e8ca8c091463e3bdc98ecb2971471f46c8bc
Palak206 <94644931+Palak206@users.noreply.github.com>
2022-12-12 19:58:43 +0530

    Time: 195 ms (96.38%), Space: 98.6 MB (69.42%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -12,34 +12,68 @@
 //     }
 // };
 
-class Solution {
-public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<int> s;
-        vector<int> ans;
-        int n = temperatures.size();
+
+
+
+// USING STACK 
+// Time Complexity: O(N) Since we move from right to left, and the inner while loop stack only visit each temperature of the temperatures vector only once in all the iterations, so it's O(N) time complexity 
+// Space Complexity: O(N) (stack)
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         stack<int> s;
+//         vector<int> ans;
+//         int n = temperatures.size();
         
-        for(int i = n-1 ; i>=0 ; i--){
-            if(s.empty()){
-                s.push(i);//index
-                ans.push_back(0);
-            }
-            else{
-                while(!s.empty() && temperatures[s.top()] <= temperatures[i]){
-                    s.pop();
-                }
-                if(s.empty()){
-                    s.push(i);//index
-                   ans.push_back(0);
+//         for(int i = n-1 ; i>=0 ; i--){
+//             if(s.empty()){
+//                 s.push(i);//index
+//                 ans.push_back(0);
+//             }
+//             else{
+//                 while(!s.empty() && temperatures[s.top()] <= temperatures[i]){
+//                     s.pop();
+//                 }
+//                 if(s.empty()){
+//                     s.push(i);//index
+//                    ans.push_back(0);
+//                 }
+//                 else{
+//                     ans.push_back(s.top()-i);
+//                     s.push(i);
+//                 }
+//             }
+//         }
+// //         we have taken it in reverse order bcox stack
+//         reverse(ans.begin() , ans.end());
+//         return ans;
+//     }
+// };
+
+
+
+// Optimized Approach
+
+class Solution{
+    public:
+        vector<int> dailyTemperatures(vector<int>& temperatures) {
+            int n = temperatures.size();
+            vector<int> ans(n , 0);
+            int warmer = INT_MIN;
+            
+            for(int i = n-1 ; i>=0 ; i--){
+                if(temperatures[i] >= warmer){
+                    warmer = temperatures[i];
                 }
                 else{
-                    ans.push_back(s.top()-i);
-                    s.push(i);
+                    int it = i+1;
+                    while(temperatures[it] <= temperatures[i]){
+                        it = it+ans[it];
+                    }
+                    ans[i] = it - i;
                 }
             }
+            
+            return ans;
         }
-//         we have taken it in reverse order bcox stack
-        reverse(ans.begin() , ans.end());
-        return ans;
-    }
 };
