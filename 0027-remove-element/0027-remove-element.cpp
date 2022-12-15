02441616176e012597f3bfa491d87b04e220e600
Palak206 <94644931+Palak206@users.noreply.github.com>
2022-12-16 00:47:14 +0530

    Time: 7 ms (26.06%), Space: 8.8 MB (37.16%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -23,26 +23,43 @@
 // };
 
 
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+        
+//         int n=nums.size();
+//            int count = 0;
+//         stack<int> s;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]!=val)
+//                 s.push(nums[i]);
+//         }
+//         nums.clear();
+//          while(!s.empty())
+//          {
+//              nums.push_back(s.top());
+//              s.pop();
+//              count++;
+//          }
+//         // return nums.size();
+//         return count;
+//     }
+// };
+
+
+// Time complexity: O(N)
+// Space complexity: O(1)
+// Code
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        
-        int n=nums.size();
-           int count = 0;
-        stack<int> s;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]!=val)
-                s.push(nums[i]);
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[j++]=nums[i];
+            }
         }
-        nums.clear();
-         while(!s.empty())
-         {
-             nums.push_back(s.top());
-             s.pop();
-             count++;
-         }
-        // return nums.size();
-        return count;
+        return j;        
     }
 };
