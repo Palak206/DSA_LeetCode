ae0c64a122cb84cff0817e9a8aa6aa1583cb2f7d
Palak206 <94644931+Palak206@users.noreply.github.com>
2022-12-16 00:43:06 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (37.16%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,43 +1,48 @@
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-        stack<int>s;
-        sort(nums.begin() , nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=val){
-                s.push(nums[i]);
-            }
-        }
-        vector<int>num;
-        int count=0;
-        while(!s.empty()){
-            count++;
-            num.push_back(s.top());
-            s.pop();
-        }
-        // reverse(num.begin(),num.end());
-        nums=num;
-        return count;
-        
-    }
-};
 // class Solution {
 // public:
 //     int removeElement(vector<int>& nums, int val) {
-        
-//         int n=nums.size();
-//         stack<int> s;
-//         for(int i=0;i<n;i++)
-//         {
-//             if(nums[i]!=val)
+//         stack<int>s;
+//         // sort(nums.begin() , nums.end());
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]!=val){
 //                 s.push(nums[i]);
+//             }
 //         }
-//         nums.clear();
-//          while(!s.empty())
-//          {
-//              nums.push_back(s.top());
-//              s.pop();
-//          }
-//         return nums.size();
+//         vector<int>num;
+//         int count=0;
+//         while(!s.empty()){
+//             count++;
+//             num.push_back(s.top());
+//             s.pop();
+//         }
+//         // reverse(num.begin(),num.end());
+//         nums=num;
+//         return count;
+        
 //     }
 // };
+
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+        int n=nums.size();
+           int count = 0;
+        stack<int> s;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=val)
+                s.push(nums[i]);
+        }
+        nums.clear();
+         while(!s.empty())
+         {
+             nums.push_back(s.top());
+             s.pop();
+             count++;
+         }
+        // return nums.size();
+        return count;
+    }
+};
