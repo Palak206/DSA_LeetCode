a91bcc00da191056494c56c9a1bbd133c210ef47
Palak206 <94644931+Palak206@users.noreply.github.com>
2022-12-26 19:19:25 +0530

    Time: 54 ms (99.20%), Space: 48.5 MB (28.86%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,16 +1,33 @@
+// from n-1 to 0 index
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         int index = n-1;
+        
+//         for(int i = n-1 ; i>=0 ; i--){
+//             if(nums[i] + i >= index){
+//                 index = i;
+//             }
+//         }
+        
+
+//         if(index == 0)  return true;
+//         return false;
+//     }
+// };
+
+
+// from o to n
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        
-        int n = nums.size();
-        int index = n-1;
-        
-        for(int i = n-1 ; i>=0 ; i--){
-            if(nums[i] + i >= index){
-                index = i;
-            }
+        int n=nums.size(), ind=0;
+        for(int i=0;i<n;i++){
+            if(ind<i)return 0;
+            ind=max(ind,i+nums[i]);
         }
-        if(index == 0)  return true;
-        return false;
+        return 1;
     }
 };
