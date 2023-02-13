0cff58f61274d0be3e8f4f274e8a0a5d9f4b0f7a
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-02-14 00:01:53 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (92.26%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,13 +1,31 @@
+// class Solution {
+// public:
+//     int countOdds(int low, int high) {
+//         int count = 0;
+//         int i ;
+//         for(i = low ; i<=high ; i++){
+//             if(i%2 != 0)
+//                 count++;
+//         }
+//         return count;
+        
+//     }
+// };
+
+
+
+
+// Total odd number between 1 and low - 1 islow/2.
+// Total odd number between 1 and high is (high + 1 ) / 2.
+// For getting answer we will do
+// Total odd number between 1 and high - Total odd number between 1 and low - 1
+// Complexity
+// Time complexity:O(1)
+// Space complexity:O(1)
+
 class Solution {
 public:
-    int countOdds(int low, int high) {
-        int count = 0;
-        int i ;
-        for(i = low ; i<=high ; i++){
-            if(i%2 != 0)
-                count++;
-        }
-        return count;
-        
+    int countOdds(int l, int h) {
+        return ((h + 1) / 2) - (l / 2);
     }
 };
