4c22404c1380c39d431a208d839bf7aba1ceb14d
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-02-19 23:52:23 +0530

    Time: 3 ms (73.66%), Space: 12.2 MB (17.21%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -17,26 +17,28 @@ public:
             return result;
         }
         
-        queue<TreeNode*>nodesQueue;
-        nodesQueue.push(root);
+        queue<TreeNode*>q;
+        q.push(root);
+        
         bool leftToRight = true;
-        while(!nodesQueue.empty()){
-            int size = nodesQueue.size();
+        
+        while(!q.empty()){
+            int size = q.size();
             vector<int>row(size);
             for(int i =0 ; i<size ; i++){
-                TreeNode* node = nodesQueue.front();
-                nodesQueue.pop();
+                TreeNode* node = q.front();
+                q.pop();
                 
 //                 find position to fill node's value
                 int index = (leftToRight) ? i : (size-1-i);
                 
                 row[index] = node->val;
                 if(node->left){
-                    nodesQueue.push(node->left);
+                    q.push(node->left);
                 }
                 
                 if(node->right){
-                    nodesQueue.push(node->right);
+                    q.push(node->right);
                 }
                 
             }
