4d8f9187da5a0461ab520a9586ffa32de47712ff
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-03-04 01:15:41 +0530

    Time: 8 ms (60.51%), Space: 16.8 MB (49.10%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -9,26 +9,83 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int sumRootToLeaf(TreeNode* root) {
+//         solve(root,0);
+//         return sum;
+//     }
+        
+//     long sum=0;
+//     void solve(TreeNode* &root,long val){
+//         //base case
+//         if(root==NULL){
+//             return;
+//         }
+//         if(root->left==NULL && root->right==NULL){
+//             sum+= (val*2 + root->val);
+//             return;
+//         }
+//         solve(root->left,val*2+root->val);
+//         solve(root->right,val*2+root->val);
+//     }
+    
+    
+// };
+
+
+// class Solution {
+// public:
+//     int sumRootToLeaf(TreeNode* root) {
+//         if(!root)
+//             return 0;
+//         solve(root,0);
+//         return sum;
+//     }
+        
+//     long sum=0;
+//     void solve(TreeNode* &root,long val){
+//         //base case
+//         if(!root)
+//             return;
+        
+//         val *= 10;
+//         val += root->val;
+//         if(!root->val && !root->right){
+//             sum += val;
+//             return;
+//         }
+//         solve(root->left, val);
+//         solve(root->right, val);
+//     }
+    
+    
+// };
+
 class Solution {
 public:
     int sumRootToLeaf(TreeNode* root) {
-        solve(root,0);
-        return sum;
+        if(!root)
+            return 0;
+        dfs(root,0);
+        return ans;
     }
-        
-    long sum=0;
-    void solve(TreeNode* &root,long val){
-        //base case
-        if(root==NULL){
+    long ans =0;
+    void dfs(TreeNode *root,long val)
+    {
+        if(!root)
+            return;
+        // val *=10;
+        val = val << 1; // left shift means val *=10;
+        val += root->val;
+        if(!root->left && !root->right)
+        {
+            ans+=val;
             return;
         }
-        if(root->left==NULL && root->right==NULL){
-            sum+= (val*2 + root->val);
-            return;
-        }
-        solve(root->left,val*2+root->val);
-        solve(root->right,val*2+root->val);
+       if(root->left) dfs(root->left,val);
+       if(root->right) dfs(root->right,val);
     }
-    
+
     
 };
