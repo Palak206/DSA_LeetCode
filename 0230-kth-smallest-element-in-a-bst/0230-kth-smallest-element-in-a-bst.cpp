60abd93a0d77d8021fb22a1dbb7e416a1306f7e4
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-03-23 00:58:07 +0530

    Time: 27 ms (17.84%), Space: 24.5 MB (6.01%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -12,26 +12,47 @@
 //inorder traversal so alll node get sorted
 // v = [1 2 3 4]
 // then v(k-1)th index = 1-1 = v[0] = 1
+
+// T.C & S.C = O(N)
+// class Solution {
+// public:
+//     vector<int>v;
+//     void inorder(TreeNode* root){
+//         if(root == NULL){
+//             return ;
+//         }
+//         inorder(root->left);
+//         v.push_back(root->val);
+//         inorder(root->right);
+        
+        
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         if(root == NULL)
+//             return NULL;
+//         inorder(root);
+//         return v[k-1];
+        
+        
+        
+//     }
+// };
+
+// T.C = O(LOGN) S.C = O(N)
 class Solution {
 public:
-    vector<int>v;
-    void inorder(TreeNode* root){
-        if(root == NULL){
-            return ;
-        }
-        inorder(root->left);
-        v.push_back(root->val);
-        inorder(root->right);
-        
-        
-    }
     int kthSmallest(TreeNode* root, int k) {
-        if(root == NULL)
-            return NULL;
-        inorder(root);
-        return v[k-1];
-        
-        
-        
+        vector <int> ans;
+        solve(root, ans);
+        sort(ans.begin(), ans.end());
+        return ans[k-1];
+    }
+
+    void solve(TreeNode* root, vector <int>& ans) {
+        if (root == NULL)
+            return;
+        ans.push_back(root->val);
+        solve(root->left, ans);
+        solve(root->right, ans);    
     }
 };
