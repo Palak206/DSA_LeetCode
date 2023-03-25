e9f7e693baacbc42bfeeb1e1f9df4f7e38506402
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-03-25 19:10:47 +0530

    Time: 3 ms (40.70%), Space: 6 MB (65.17%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -7,26 +7,26 @@
 
 // without +, - oerator
 // using bit manipulation
-// class Solution {
-// public:
-//     int getSum(int a, int b) {
-//         unsigned int buff;
-//         do {
-//             buff = a & b; // 1 & 2(10) = 0
-//             a ^= b;  // 1^2 = 11  = 3
-//             b = buff << 1; // b = 0 << 1 = 0*(2^1) = 0
-//         } while(buff);  // so bufff is 0 ans is 3
-//         return a;
-//     }
-// };
+class Solution {
+public:
+    int getSum(int a, int b) {
+        unsigned int buff;
+        do {
+            buff = a & b; // 1 & 2(10) = 0
+            a ^= b;  // 1^2 = 11  = 3
+            b = buff << 1; // b = 0 << 1 = 0*(2^1) = 0
+        } while(buff);  // so buff is 0 ans is 3
+        return a;
+    }
+};
 
 // using logarithmic
-class Solution{
-    public:
-          int getSum(int a, int b){
-              return log2(std::pow(2, a) * std::pow(2, b));
-          }
+// class Solution{
+//     public:
+//           int getSum(int a, int b){
+//               return log2(std::pow(2, a) * std::pow(2, b));
+//           }
     
-};
+// };
 
 // log2(2^a * 2^b) = log2(2^a)+log2(2^b) = log2(2^1) + log2(2^2) = 1(log2(2))+ 2(log2(2)) = 1+2 = 3
