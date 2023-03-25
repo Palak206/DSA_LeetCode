08ad6390ff69eaac93768f8091b4fe3bfc331fed
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-03-25 12:47:05 +0530

    Time: 347 ms (43.62%), Space: 103.2 MB (46.90%) - LeetHub

---
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -0,0 +1,50 @@
+// formula childAns = parentAns - x +(n-x)
+// x is a connected subtree of child 
+//  for ex 0 ka ans = 8
+// and for node 2 = 8(parent of 2 that is 0 and that ans is 10) - 4(connected node of 2) + (6-4) = 8- 4+2 = 6
+// T.C & S.C = O(N)
+
+class Solution {
+    int size;
+public:
+    vector<int> ans; //to store answers
+    vector<int> cnt;  //to store count of childrens on subtree of particular node
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        size=n;
+        vector<int> adj[n];
+        ans.resize(n,0);
+        cnt.resize(n,1);
+        for(auto x:edges){
+            adj[x[0]].push_back(x[1]);
+            adj[x[1]].push_back(x[0]);
+        }
+        
+        dfs(0,-1,adj); //to calculate child count and anwer w.r.t. child
+        dfs2(0,-1,adj); //to calculate answer with respect to parents
+        return ans;
+    }
+    void dfs(int src,int par,vector<int> adj[])
+    {
+        for(auto& child:adj[src])
+        {
+            if(child!=par)
+            {
+                dfs(child,src,adj);
+                cnt[src]+=cnt[child];
+                ans[src]+=ans[child]+cnt[child];
+            }
+        }
+    }
+    void dfs2(int src,int par,vector<int> adj[])
+    {
+        for(auto& child:adj[src])
+        {
+            if(child!=par)
+            {
+                 ans[child]+=(ans[src]+(size-cnt[child])-ans[child]-cnt[child]);
+                //ans[child]+=(ans w.r.t. parent+total number of nodes in parents' subtree excluding child's subtree)-(child's contribution in parents's answer)
+                 dfs2(child,src,adj);
+            }
+        }
+    }
+};
