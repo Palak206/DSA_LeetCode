6d2574a2377e1428daeec7fae70ca5b06d9562de
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-03-31 00:49:30 +0530

    Time: 4 ms (93.05%), Space: 21 MB (99.46%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -60,48 +60,80 @@
 //     }
 // };
 
-class Solution {
-public:
-   int cam;
-   set<TreeNode*>covered;
+// T.C & S.C = O(N)
+// class Solution {
+// public:
+//    int cam;
+//    set<TreeNode*>covered;
    
-   void dfs(TreeNode*node, TreeNode*parent){
-       if(node!=NULL){  
-           dfs(node->left, node);
-           dfs(node->right, node);
+//    void dfs(TreeNode*node, TreeNode*parent){
+//        if(node!=NULL){  
+//            dfs(node->left, node);
+//            dfs(node->right, node);
            
-           //we are at the depth of the tree now we want to check weather
-           //we need camera at the node
-           //check if parent is null or uncovered
-           // OR
-           // any of its child left or right is uncovered
-           if(
-               (parent == NULL && covered.find(node) == covered.end()) ||
-               (covered.find(node->left) == covered.end()) ||
-               (covered.find(node->right) == covered.end())
-           ){
-               cam++;
-               covered.insert(node);
-               covered.insert(parent);
-               covered.insert(node->left);
-               covered.insert(node->right);
-           }
-       }
-   }
+//            //we are at the depth of the tree now we want to check weather
+//            //we need camera at the node
+//            //check if parent is null or uncovered
+//            // OR
+//            // any of its child left or right is uncovered
+//            if(
+//                (parent == NULL && covered.find(node) == covered.end()) ||
+//                (covered.find(node->left) == covered.end()) ||
+//                (covered.find(node->right) == covered.end())
+//            ){
+//                cam++;
+//                covered.insert(node);
+//                covered.insert(parent);
+//                covered.insert(node->left);
+//                covered.insert(node->right);
+//            }
+//        }
+//    }
    
-   int minCameraCover(TreeNode* root) {
-       if(root == NULL) return 0; // no node no camera required;
+//    int minCameraCover(TreeNode* root) {
+//        if(root == NULL) return 0; // no node no camera required;
        
-       //I have inserted NULL to the covered because we don't want
-       //camera to be installed at the leaf nodes
-       covered.insert(NULL);
-       cam = 0;
+//        //I have inserted NULL to the covered because we don't want
+//        //camera to be installed at the leaf nodes
+//        covered.insert(NULL);
+//        cam = 0;
        
-       // Now we will pass root and a NULL pointer as a argument
-       // NULL pointer points to the parent node of the current node
-       // in our case as root as no parent we will pass NULL
+//        // Now we will pass root and a NULL pointer as a argument
+//        // NULL pointer points to the parent node of the current node
+//        // in our case as root as no parent we will pass NULL
        
-       dfs(root,NULL);
-       return cam;
-   }
+//        dfs(root,NULL);
+//        return cam;
+//    }
+// };
+
+class Solution{
+    int cam;
+    public:
+          int minCameraCover(TreeNode* root) {
+              cam = 0;
+             int ans = dfs(root);
+              //  if 0 (uncovered) then cam+1
+              //if (1 ,2) node covered then return the no. of camera that have added in the tree
+              return ans == 0 ? cam+1 : cam;
+          }
+    
+    int dfs(TreeNode* node){
+        // 0 for uncovered node
+        //1 for node covered with camera
+        // 2 for node with camera
+        if(node == NULL) return 1;
+        
+        int left = dfs(node->left);
+        int right = dfs(node->right);
+        
+        if(left == 0 || right == 0){
+            cam++;
+            return 2; //camera
+        }
+        else if(left == 2 || right == 2)
+            return 1;//covered
+        else
+            return 0; //uncovered
+    }
 };
