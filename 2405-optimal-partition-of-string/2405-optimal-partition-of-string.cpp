1226988f590c883a34e460bb9bc3ec3a93d4faa1
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-04-04 11:39:42 +0530

    Time: 15 ms (98.68%), Space: 10.3 MB (77.04%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -8,23 +8,45 @@
 //   a|ab|     ac|       a = 4
 
 
-
-class Solution {
-public:
-    int partitionString(string s) {
-        unordered_set<char>st;
-        int count = 1; //count of no of sub-str so far at every partition ,one substring would increase
-        for(int i = 0 ; i<s.size() ; i++){
-            if(st.find(s[i]) != st.end()){ 
-                // means "aba" a is repeating at first and at last
-                count++;
-                // reset the set ab| a whwnever partion came
-                st.clear();
+// T.C = O(N) , S.C = O(26) = 26 char 
+// class Solution {
+// public:
+//     int partitionString(string s) {
+//         unordered_set<char>st;
+//         int count = 1; //count of no of sub-str so far at every partition ,one substring would increase
+// //         if we use partion in place of we start from and in answer we write it partion+1 
+// //         if there are n partion so n+1 substring are there 
+//         for(int i = 0 ; i<s.size() ; i++){
+//             if(st.find(s[i]) != st.end()){ 
+//                 // means "aba" a is repeating at first and at last
+//                 count++;
+//                 // reset the set ab| a whenever partion came
+//                 st.clear();
                 
-            }
-            st.insert(s[i]);
-        }
-        return count;
+//             }
+//             st.insert(s[i]); // upcoming char would for sure go in
+//         }
+//         return count;
         
-    }
+//     }
+// };
+
+
+// Bit -> t.c = o(n) s.c = o(1)
+class Solution{
+    public:
+          int partitionString(string s){
+              int i = 0, ans = 1, flag = 0;
+              while(i < s.size()){
+                  int val = s[i] - 'a';
+                  if(flag & (1<<val)){
+                      flag = 0;
+                      ans++;
+                  }
+                  flag = flag | (1<<val); //-> set the bit of that character
+                  i++;
+              }
+              return ans;
+          }
+    
 };
