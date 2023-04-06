68faaf29763451f3807a9f6f08ec5d6e15af750b
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-04-07 00:32:05 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (38.34%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,12 +1,31 @@
+// class Solution {
+// public:
+//     uint32_t reverseBits(uint32_t n) {
+//         uint32_t ans = 0;
+//         int d = 0;
+//         while(n>0){
+//             ans += (n%2) << (31-d);
+//             n >>= 1;
+//             d++;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
+    bool findKthbitset(int k , uint32_t n){
+        if(((1 << (k-1) & n) != 0)) return true;
+        return false;
+    }
+           
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans = 0;
-        int d = 0;
-        while(n>0){
-            ans += (n%2) << (31-d);
-            n >>= 1;
-            d++;
+        for(int i = 1 ; i<=32 ; i++){
+            if(findKthbitset(i , n)){ //check current set is 1 or not
+                //set a bit
+                ans = 1 << (32-i) | ans;
+            }
         }
         return ans;
     }
