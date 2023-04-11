81168c54e0a7be186fbc09dc9d21502f8b096913
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-04-11 20:12:09 +0530

    Time: 71 ms (94.50%), Space: 24.1 MB (96.51%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,25 +1,44 @@
+// // T.C = O(N) & S.C = O(N)
+
+// class Solution {
+// public:
+//     string removeStars(string s) {
+//         stack<char>st;
+//         for(int i =  0; i<s.length() ; i++){
+//             if(s[i] == '*'){
+//                 st.pop();
+//             }
+//             else{
+//                 st.push(s[i]);
+//             }
+//         }
+// // we Store all the elements of stack in a variable.
+// // and we have performed this operation in stack therfore the obtained string will be in reverse order.
+// // at the end we Reverse the string and return it as answer
+//         string ans = "";
+//         while(!st.empty()){
+//             ans += st.top();
+//             st.pop();
+            
+//         }
+//         reverse(ans.begin() , ans.end());
+//         return ans;
+//     }
+// };
+
+
+// TWO POINTER T.C = O(N) S.C = O(1)
 class Solution {
 public:
     string removeStars(string s) {
-        stack<char>st;
-        for(int i =  0; i<s.length() ; i++){
-            if(s[i] == '*'){
-                st.pop();
-            }
-            else{
-                st.push(s[i]);
+        int i=0,j=0;
+        for(i=0;i<s.size();i++){
+            if(s[i]=='*'){
+                j--;
+            }else{
+                s[j++] = s[i];
             }
         }
-// we Store all the elements of stack in a variable.
-// and we have performed this operation in stack therfore the obtained string will be in reverse order.
-// at the end we Reverse the string and return it as answer
-        string ans = "";
-        while(!st.empty()){
-            ans += st.top();
-            st.pop();
-            
-        }
-        reverse(ans.begin() , ans.end());
-        return ans;
+        return s.substr(0,j);
     }
 };
