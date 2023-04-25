8c5bda96a365b5b5ac6722b593c840f424fc5754
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-04-25 11:15:35 +0530

    Time: 75 ms (88.87%), Space: 35.4 MB (92.24%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -1,50 +1,50 @@
-class SmallestInfiniteSet {
-public:
-    vector<bool> v;
-    SmallestInfiniteSet() {
-        v.resize(1000,1); //1 for true
-    }
-    
-    int popSmallest() {
-        for(int i = 0  ; i<1000 ; i++){
-            if(v[i] == 1){
-                v[i] = 0;
-                return i+1;
-            }
-        }
-        return -1;
-    }
-    
-    void addBack(int num) {
-        v[num-1] = 1;
-    }
-};
-
-
 // class SmallestInfiniteSet {
 // public:
-//     int curr ;
-//     set<int>set;
-//     //initialize empty set
+//     vector<bool> v;
 //     SmallestInfiniteSet() {
-//         curr = 1;
+//         v.resize(1000,1); //1 for true
 //     }
     
 //     int popSmallest() {
-//         if(s.size()){ // if set is not empty
-//             int res=*s.begin(); s.erase(res);
-//             return res;
-//         }else{
-//             curr++; //if set is empty
-//             return curr-1;
+//         for(int i = 0  ; i<1000 ; i++){
+//             if(v[i] == 1){
+//                 v[i] = 0;
+//                 return i+1;
+//             }
 //         }
+//         return -1;
 //     }
     
 //     void addBack(int num) {
-//         if(curr > num)
-//             s.insert(num);
+//         v[num-1] = 1;
 //     }
 // };
+
+
+class SmallestInfiniteSet {
+public:
+    int curr ;
+    set<int>s;
+    //initialize empty set
+    SmallestInfiniteSet() {
+        curr = 1;
+    }
+    
+    int popSmallest() {
+        if(s.size()){ // if set is not empty
+            int res=*s.begin(); s.erase(res);
+            return res;
+        }else{
+            curr++; //if set is empty
+            return curr-1;
+        }
+    }
+    
+    void addBack(int num) {
+        if(curr > num)
+            s.insert(num);
+    }
+};
 
 /**
  * Your SmallestInfiniteSet object will be instantiated and called as such:
