c70e0a37f1cf8585306dc420d92aafc3fa6c41f7
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-04-26 11:29:26 +0530

    Time: 13 ms (97.56%), Space: 13.4 MB (76.11%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool topoSort(int s,vector<bool> &visited,vector<bool> &curr_visited, vector<int>adjList[] ){
+        visited[s] = true;
+        curr_visited[s] = true;
+        
+        // vector<int>data = neighbour;
+            for(auto neighbour : adjList[s]){
+                  if(!visited[neighbour]){
+                      if(topoSort(neighbour, visited, curr_visited, adjList)){
+                          return true;
+                      }
+                  }
+                else if(visited[neighbour] && curr_visited[neighbour] ){
+                    return true;
+                }
+              }
+              // whenever we go back to the node we push
+              //  that node in the stack
+             curr_visited[s] = false;
+             return false;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n= numCourses;
+        vector<bool> visited(n ,false) ,curr_visited(n,false);
+        
+        vector<int>adjList[n];
+        for(int i = 0; i<prerequisites.size() ; i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+           //directed acyclic graph
+           adjList[u].push_back(v);
+        }
+        for(int i = 0; i<n ; i++){
+            if(!visited[i]){
+                if(topoSort(i,visited,curr_visited,adjList)){
+                      return false;//if cycle present
+                }
+            }
+                
+        }
+        return true;
+
+    }
+};
