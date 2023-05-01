fb2ab49439be1d49aa32d240c53fbfdae5daee26
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-05-02 00:11:54 +0530

    Time: 260 ms (37.52%), Space: 10.3 MB (70.94%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,39 @@
+// class Solution {
+// public:
+//     bool isPrime(int n) {
+//         int n_ = int(sqrt(n));
+//         for (int i = 2; i <= n_; i++) {
+//             if (n % i == 0) {
+//                 return 0;
+//             }
+//         }
+//         return 1;
+//     }
+//     int countPrimes(int n) {
+//         int count = 0;
+//         for(int i = 2 ; i<n ; i++){
+//             if(isPrime(i)){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+class Solution {
+public:
+    int countPrimes(int n) {
+        int count =0;
+        vector<bool> prime(n+1,true);
+        prime[0]=prime[1]=false;
+        for(int i=2;i<n;i++){
+            if(prime[i]){
+                count++;
+
+                for(int j=2*i;j<n;j=j+i){
+                    prime[j]=0;
+                }
+            }
+        }
+        return count;
+    }
+};
