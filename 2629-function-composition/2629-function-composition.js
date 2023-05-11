9ad2fbe8d281293762bd54fea2b3ef9777e3bce1
Palak206 <94644931+Palak206@users.noreply.github.com>
2023-05-11 23:34:10 +0530

    Time: 64 ms (90.42%), Space: 43.4 MB (41.46%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -18,6 +18,14 @@ var compose = function(functions) {
     }
 };
 
+// var compose = function(functions) {
+//   return function(x) {
+//     return functions.reduceRight(function(acc, fn) {
+//       return fn(acc);
+//     }, x);
+//   }
+// };
+
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
