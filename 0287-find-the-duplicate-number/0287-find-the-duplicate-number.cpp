31ca2bf6b23e1f53e8db0cb1276ca1dad224e386
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-07-02 11:11:08 +0530

    Time: 147 ms (43.39%), Space: 61.2 MB (44.75%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,23 +1,52 @@
-class Solution {
-public:
-    int findDuplicate(vector<int>& nums) {
-        int slow = nums[0];
-		int fast = nums[nums[0]];
-		while (slow != fast)
-		{
-			slow = nums[slow];
-			fast = nums[nums[fast]];
-		}
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         vector<int> cnt(nums.size(),0);
+//         int ind =  0;
+		
+// 		// store the cnt of each value in the cnt vector
+//         for(int i = 0; i<nums.size(); i++)
+//         {
+//             cnt[nums[i]]++;
+//         }
+        
+//         for(int i = 0; i<cnt.size(); i++)
+//         {
+// 			// if cnt[i] > 1
+// 			// this means that element occur more than once in nums
+// 			// we have to return i
+//             if(cnt[i] > 1)
+//             {
+//                 ind  = i;
+//                 break;
+//             }
+//         }
+        
+//         return ind;
+//     }
 
-		fast = 0;
-		while (fast != slow)
-		{
-			fast = nums[fast];
-			slow = nums[slow];
-		}
-		return slow;
-	
-	return -1;
-    }     
-    
+// };
+
+
+class Solution {
+    public:
+    int findDuplicate(vector<int>& nums){
+        int low = 0,high = nums.size() - 1, cnt;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            cnt = 0;
+            for(int num:nums){
+                if(num <= mid){
+                    cnt++;
+                }
+            }
+            if(cnt <= mid){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+    return low; 
+    }
+   
 };
