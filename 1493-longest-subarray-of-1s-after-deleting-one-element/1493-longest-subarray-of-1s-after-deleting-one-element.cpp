afae7cc387c1fb0e3ecbf26fe653c04c1ca89eca
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-07-06 00:23:49 +0530

    Time: 48 ms (47.11%), Space: 36.5 MB (19.62%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,20 +1,42 @@
 // (head-tail) for deleted element
+// T.C = O(N^2) & S.C = O(1)
+// class Solution {
+// public:
+//     int longestSubarray(vector<int>& nums) {
+//         int i =0, j=0, res=0, count=0;
+        
+//         for(i =0; i<nums.size() ; i++){
+//             if(nums[i] == 0)
+//                 count++;
+            
+//             while(count > 1){// that means we got 0 element
+//                 if(nums[j] == 0) count--;//valid case delete element
+//                 j++;
+                
+//             }
+//             res = max(res,i-j);
+//         }
+//         return res;
+//     }
+// };
+
+// T.C = O(N)
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int i =0, j=0, res=0, count=0;
-        
-        for(i =0; i<nums.size() ; i++){
-            if(nums[i] == 0)
-                count++;
-            
-            while(count > 1){// that means we got 0 element
-                if(nums[j] == 0) count--;//valid case
-                j++;
-                
+        int n = nums.size();
+        int ans = 0;
+        int i = 0, cnt = 0,prev = -1;
+        while(i < n){
+            if(nums[i] == 0){
+                ans = max(ans, cnt);
+                cnt = i - prev - 1;
+                prev = i;
             }
-            res = max(res,i-j);
+            else cnt++;
+            i++;
         }
-        return res;
+        ans = max(ans, cnt);
+        return cnt == n? ans-1 : ans; 
     }
 };
