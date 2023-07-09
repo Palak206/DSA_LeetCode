f0c75396601769b5dd7038815509a7717197bd4f
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-07-09 11:25:32 +0530

    Time: 38 ms (80.72%), Space: 27.4 MB (84.26%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,44 +1,53 @@
-// T.C.= O(logn) using binary search
+// // T.C.= O(logn) using binary search
 
-class Solution {
-public:
-    int binary(vector<int>v,int l, int r, int key)
-    {
-		// till l doesn't exceed r
-        if(r >= l)
-        {
-            int mid = (l+r)/2;
+// class Solution {
+// public:
+//     int binary(vector<int>v,int l, int r, int key)
+//     {
+// 		// till l doesn't exceed r
+//         if(r >= l)
+//         {
+//             int mid = (l+r)/2;
 			
-			// if key equals mid return mid
-            if(key == v[mid])
-                return mid;
+// 			// if key equals mid return mid
+//             if(key == v[mid])
+    				
+// 			// if key is less than mid
+// 			// recursive call for left half
+//             else if(key < v[mid])
+//                 return binary(v,l,mid-1,key);
 				
-			// if key is less than mid
-			// recursive call for left half
-            else if(key < v[mid])
-                return binary(v,l,mid-1,key);
-				
-			// key is greater than mid
-			// recursive call for right half
-            else
-                return binary(v,mid+1,r,key);
+// 			// key is greater than mid
+// 			// recursive call for right half
+//             else
+//                 return binary(v,mid+1,r,key);
+//         }
+		
+// 		// if key not found
+//         return -1;
+//     }
+    
+    
+//     int search(vector<int>& nums, int target) {
+//         int result = binary(nums,0,nums.size()-1,target);
+		
+// 		// if func returns -1 key doesn't exists
+//         if (result == -1)
+//             return -1;
+		
+// 		// return index of key
+//         else
+//             return result;   
+//     }
+// };
+
+class Solution{
+    public:
+    int search(vector<int>& nums, int target){
+        for(int i=0 ; i<nums.size() ; i++){
+            if(nums[i] == target)
+                return i;
         }
-		
-		// if key not found
-        return -1;
+        return  -1;
     }
-    
-    
-    int search(vector<int>& nums, int target) {
-        int result = binary(nums,0,nums.size()-1,target);
-		
-		// if func returns -1 key doesn't exists
-        if (result == -1)
-            return -1;
-		
-		// return index of key
-        else
-            return result;   
-    }
-	// for github repository link go to my profile.
 };
