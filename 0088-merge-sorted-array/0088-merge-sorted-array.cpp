93b6fe0bf1bd5b26313746ee6b51dd6045361258
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-07-10 11:16:08 +0530

    Time: 3 ms (60.99%), Space: 9 MB (80.96%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,3 +1,4 @@
+// t.c = o(mlogm) s.c = o(1)
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -14,3 +15,28 @@ public:
         sort(nums1.begin(), nums1.end());
     }
 };
+
+// using 2 pointers
+// t.c = o(m+n) s.c = o(1)
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         int i = m-1;
+//         int j = n-1;
+//         int k = m+n-1;
+//         while(i>=0 && j>=0){
+//             if(nums1[i] > nums2[j]){
+//                 nums1[k] = nums1[i];
+//                 i--; k--;
+//             }
+//             else{
+//                 nums1[k] = nums2[j];
+//                 j--; k--;
+//             }
+//         }
+//         while(j>=0){
+//             nums1[k] = nums2[j];
+//             j--; k--;
+//         }
+//     }
+// };
