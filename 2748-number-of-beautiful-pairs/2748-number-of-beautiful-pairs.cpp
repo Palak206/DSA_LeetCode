88dfa56f548f8011d8287a6fe77e9b10b3c9484f
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-07-13 19:19:04 +0530

    Time: 65 ms (66.05%), Space: 67.5 MB (78.58%) - LeetHub

--- 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.cpp
+++ 2748-number-of-beautiful-pairs/2748-number-of-beautiful-pairs.cpp
@@ -1,29 +1,52 @@
-class Solution {
-public:
-    int gcd(int n1 , int n2){
-        while (n2 != 0) {
-            int res = n2;
-            n2 = n1 % n2;
-            n1 = res;
-        }
-        return n1;
-    }
+// class Solution {
+// public:
+//     int gcd(int n1 , int n2){
+//         while (n2 != 0) {
+//             int res = n2;
+//             n2 = n1 % n2;
+//             n1 = res;
+//         }
+//         return n1;
+//     }
     
-    int countBeautifulPairs(vector<int>& nums) {
-        int n = nums.size() , ans = 0;
+//     int countBeautifulPairs(vector<int>& nums) {
+//         int n = nums.size() , ans = 0;
         
-        for(int i=0 ; i<n ; i++){
-            for(int j = i+1 ; j<n ; j++){
-                string s1 = to_string(nums[i]);
-                string s2 = to_string(nums[j]);
+//         for(int i=0 ; i<n ; i++){
+//             for(int j = i+1 ; j<n ; j++){
+//                 string s1 = to_string(nums[i]);
+//                 string s2 = to_string(nums[j]);
                 
-                int n1 = s1[0] - '0'; 
-                int n2 = s2.back() - '0';
+//                 int n1 = s1[0] - '0'; 
+//                 int n2 = s2.back() - '0';
                 
-                if (gcd(n1,n2) == 1)
-                    ans++;
-            }
-        }
-        return ans;
-    }
+//                 if (gcd(n1,n2) == 1)
+//                     ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+class Solution{
+    public:
+         int countBeautifulPairs(vector<int>& nums){
+             int n = nums.size() , ans = 0;
+             
+             for(int i=0 ; i<n ; i++){
+                 int n1 = nums[i] , n2;
+                 while(n1>0){
+                     n2 = n1%10;
+                     n1/=10;
+                 }
+                 
+                 for(int j=i+1; j<n ; j++){
+                     if(__gcd(n2, nums[j]%10) == 1)
+                         ans++;
+                 }
+             }
+             return ans;
+         }
+    
 };
