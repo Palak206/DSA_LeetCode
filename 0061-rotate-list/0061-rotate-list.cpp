bd4cdf7453031fe708d9805f215d2c32a029110b
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-07-15 23:51:45 +0530

    Time: 7 ms (75.40%), Space: 11.7 MB (61.06%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,11 +11,14 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL || head->next==NULL)
-        return head;
-        int cnt = 1;
+        if(head==NULL || k == 0)
+          return head;
+        
+        int cnt = 0;
         ListNode* temp = head;
         // count the no. of node
+        //reach the last elemet after rotating
+        // our last element is connected to head (5->1)
         while(temp->next!=NULL)
         {
             temp=temp->next;
@@ -23,21 +26,28 @@ public:
         }
 //         if k= 0 then we don't reverse it we return as it as
         
-          k=k%cnt;
-          if(k==0) return head;
-
+        
+         //2nd test case size = 2 and k= 4 if we rotate it 4 times
+        //the result will same as that we get in 1st times
+        
+          k=k%(cnt+1);
+        // temp = 5 last element
+        // now we connect it's next to the head 5->1
         temp->next = head;
         
-        int res = cnt - k;
+        //cnt = 4
+        int jump = cnt - k;//4-2= 2  
         temp = head;
-        int i=1;
-        while(i<res)
+        // int i=1;
+        while(jump)
         {
             temp=temp->next;
-            i++;
+            jump--;
         }
-        ListNode* prev = temp->next;
-        temp->next = NULL;
+         // temp = 3
+        //we want to save 4->5
+        ListNode* prev = temp->next; // 4
+        temp->next = NULL; // 3 ->null
         return prev;
     }
 };
