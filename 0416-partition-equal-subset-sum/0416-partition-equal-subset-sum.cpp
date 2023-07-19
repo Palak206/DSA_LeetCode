20fd300ace9783db71381198e718bc8beeb575e3
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-07-19 11:12:11 +0530

    Time: 563 ms (15.85%), Space: 179.3 MB (9.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -17,6 +17,7 @@
 
 class Solution {
    public:
+    //TLE
     bool solve(int index , vector<int>& nums ,int target){
         //base case
         if(index >= nums.size())
@@ -37,7 +38,7 @@ class Solution {
         
         return incl || excl;
     }
-    
+    //TOP-DOWN APPROACH total/2 -> 0
      bool solveMem(int index , vector<int>& nums ,int target, vector<vector<int>>&dp){
         //base case
         if(index >= nums.size())
@@ -55,27 +56,53 @@ class Solution {
         bool incl = solveMem(index+1 ,nums , target - nums[index],dp);//11-1= 10
         bool excl = solveMem(index+1 , nums , target -0, dp);//11-0
         
-        return dp[index][target] = incl or excl;;
+        return dp[index][target] = incl or excl;
+    }
+    
+    //BOTTOM-UP 0->total/2 n->0
+    bool solveTab(vector<int>& nums , int total){
+        vector<vector<int>> dp(nums.size()+1, vector<int>(total+1, 0));
+        
+        for(int i=0 ; i<=nums.size() ; i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int index = nums.size()-1  ; index >=0 ; index--){
+            for(int target = 0 ; target <= total/2; target++){
+                  bool incl = 0;
+                if(target - nums[index] >= 0)
+                   incl = dp[index+1][target - nums[index]];
+                //11-1= 10
+                 bool excl = dp[index+1][target - 0];//11-0
+        
+                  dp[index][target] = incl or excl;;
+            }
+        }
+        return dp[0][total/2];
     }
     
     bool canPartition(vector<int>& nums){
         int n= nums.size();
-        int sum =0;
+        int total =0;
         
         for(int i=0 ; i<n ; i++){
-            sum += nums[i];
+            total += nums[i];
         }
         
-        if(sum%2 != 0) return false;
+        if(total%2 != 0) return false;
         
-        int target = sum/2; //11
+        // int target = total/2; //11 
+        int t = total/2;
         
         for(int i=0 ;i<n ; i++){
             
         }
         
         // return solve(0 , nums , target);
-        vector<vector<int>> dp(n, vector<int>(target+1, -1));
-        return solveMem(0, nums , target , dp);
+        
+        // vector<vector<int>> dp(n, vector<int>(target+1, -1));
+        // return solveMem(0, nums , target , dp);
+        
+        return solveTab(nums , total);
     }
 };
