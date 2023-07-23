58a0f7d200e282832b9209c2e46ca7e8957de17e
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-07-23 12:06:26 +0530

    Time: 69 ms (20.32%), Space: 22.2 MB (46.14%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,3 +1,29 @@
+//  divisior will go from 1 to max element(9)
+// To calculate the result, we will iterate over the given array using a loop. Within this loop, we will divide each element in the array by the current divisor, d, and sum up the obtained ceiling values.
+// Inside the outer loop, If result <= threshold: We will return d as our answer.
+// Finally, if we are outside the nested loops, we will return -1.
+// O(N2)
+// class Solution{
+//     public:
+//     int smallestDivisor(vector<int>& nums, int threshold){
+//          int n = nums.size(); //size of array.
+//          //Find the maximum element:
+//          int maxi = *max_element(arr.begin(), arr.end());
+     
+//          //Find the smallest divisor:
+//          for (int d = 1; d <= maxi; d++) {
+//              //Find the summation result:
+//              int sum = 0;
+//              for (int i = 0; i < n; i++) {
+//                  sum += ceil((double)(arr[i]) / (double)(d));
+//              }
+//              if (sum <= limit)
+//                  return d;
+//          }
+//          return -1;
+//      }
+// };
+
 // Binary search 1 to n
 class Solution {
 public:
@@ -14,15 +40,16 @@ public:
     int smallestDivisor(vector<int>& nums, int threshold) {
         int n = nums.size();
         int start = 1;
-        int end = INT_MAX;
+        int end = INT_MAX; //*max_elemnt(nums.begin() , nums.end())
         
         int ans = 0;
         
+        if(n > threshold) return -1;
         while(start <= end){
             int mid = start+(end-start)/2;
             
             if(helper(nums , threshold , mid)){ //true but we don't know which smallest 
-                //val we get
+                //val we get ans = mid
                 end = mid-1;
                 
             }
