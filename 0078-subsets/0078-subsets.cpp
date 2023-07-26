3ea07dfb558960c06e672fe612549f628a679904
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-07-27 01:11:45 +0530

    Time: 0 ms (100.00%), Space: 7 MB (91.55%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void solve(int i , vector<int>&nums ,vector<int>&temp){
+    // vector<vector<int>> ans; using global var
+    void solve(int i , vector<int>&nums ,vector<int>&temp , vector<vector<int>>& ans){
         if(i == nums.size()){
             ans.push_back(temp);
         }
         else{
             // to take the ith ele
             temp.push_back(nums[i]);
-            solve(i+1 , nums , temp);//if element is  present
+            solve(i+1 , nums , temp, ans);//if element is  present
             
             
             temp.pop_back();
             //to ignore the ith ele
-            solve(i+1 , nums , temp);//if element is not present
+            solve(i+1 , nums , temp , ans);//if element is not present
         }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
         vector<int> temp;
-        solve(0, nums , temp);
+        solve(0, nums , temp, ans);
         return ans;
     }
 };
