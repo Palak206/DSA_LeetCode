35a44ff8e041296609d4f9bf33fddef3f9646a61
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-07-30 11:33:19 +0530

    Time: 163 ms (99.27%), Space: 102.6 MB (90.23%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,40 +1,40 @@
-// class Solution {
-// public:
-//     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-//         sort(asteroids.begin() , asteroids.end());
-//         int n = asteroids.size();
-//         // int sum = 0;
-        
-//         for(int i=0 ; i<n ; i++){
-//             if(mass < asteroids[i]){
-//                 return false;
-                
-//             }
-//             else
-//                 mass += asteroids[i];
-//         }
-//         return true;
-        
-//     }
-// };
-
 class Solution {
 public:
-
-    bool asteroidsDestroyed(long long mass, vector<int>& ass) {
+    bool asteroidsDestroyed(long long mass, vector<int>& asteroids) {
+        sort(asteroids.begin() , asteroids.end());
+        int n = asteroids.size();
+        // int sum = 0;
         
-        sort(ass.begin(), ass.end());
+        for(int i=0 ; i<n ; i++){
+            if(mass >= asteroids[i]){
+                mass += asteroids[i];
 
-        for(int i = 0; i < ass.size(); ++i)
-        {
-           if(mass < ass[i]) return false;
-           
-           else mass += ass[i];   
+            }
+            else
+                return false;
         }
-
         return true;
+        
     }
 };
+
+// class Solution {
+// public:
+
+//     bool asteroidsDestroyed(long long mass, vector<int>& ass) {
+        
+//         sort(ass.begin(), ass.end());
+
+//         for(int i = 0; i < ass.size(); ++i)
+//         {
+//            if(mass < ass[i]) return false;
+           
+//            else mass += ass[i];   
+//         }
+
+//         return true;
+//     }
+// };
 
 
 
