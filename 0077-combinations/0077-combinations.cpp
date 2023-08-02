a002a0c1a9904c02a48a0f8393166c34d5ec62cb
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-08-02 11:51:23 +0530

    Time: 25 ms (79.12%), Space: 9.9 MB (50.16%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> result;
     
-    void solve(int start , int n, int k , vector<int>&temp){
+    void solve(int start , int& n, int k , vector<int>&temp){
         if(k == 0){
             result.push_back(temp);
             return;
@@ -10,11 +10,17 @@ public:
         if(start > n)
             return ;
         
-        temp.push_back(start);
-        solve(start+1 , n , k-1 , temp);
+//         temp.push_back(start);
+//         solve(start+1 , n , k-1 , temp);
         
-        temp.pop_back();
-        solve(start+1, n , k , temp);
+//         temp.pop_back();
+//         solve(start+1, n , k , temp);
+        for(int i=start; i<=n ; i++){
+            temp.push_back(i);
+            solve(i+1 , n , k-1 , temp);
+            temp.pop_back();
+        }
+        
     }
     vector<vector<int>> combine(int n, int k) {
         vector<int>temp;
