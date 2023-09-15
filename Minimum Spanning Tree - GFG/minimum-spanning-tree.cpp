7fb909c437b324643293bf09ddc40c3e1bbbb002
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-09-15 02:51:19 -0400

    Added solution - LeetHub

---
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    typedef pair<int,int> P;
+	public:
+	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+        priority_queue<P, vector<P>, greater<P>> pq; //min-heap
+        //{wt,node,parent} but in que {wt,node}
+        
+        pq.push({0,0});
+        vector<bool> inMST(V, false);
+        int sum =0;
+        
+        while(!pq.empty()){
+            auto p = pq.top();
+            pq.pop();
+            
+            int wt = p.first;
+            int node = p.second;
+            
+            if(inMST[node] == true)
+                 continue;
+            
+            inMST[node] = true; // inMST added to oue mst
+            sum += wt;
+            
+            for(auto &tmp : adj[node]){
+                
+                int neighbour = tmp[0];
+                int neighbour_wt = tmp[1];
+                
+                if(inMST[neighbour] == false){
+                    //if not visited
+                    pq.push({neighbour_wt, neighbour});
+                }
+            }
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        
+        Solution obj;
+    	cout << obj.spanningTree(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
