0a5960f250b57d70e1d9e001bda67bc129488e16
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-09-18 01:42:17 -0400

    Time: 13 ms (31.65%), Space: 11.1 MB (9.15%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,74 @@
+// class Solution {
+// public:
+    
+//     vector<int> Weakest(vector<vector<int> >& mat, int k)
+//     {
+       
+//         int n = mat.size();
+        
+     
+//         // count of 1 in ith row, {count,index};
+//         set<pair<int,int> > s;
+        
+//         // set will automatically sort the pair {count, index} in ascending order.
+        
+//         for(int i = 0; i<n; ++i)
+//         {
+//             // stl function for counting frequency of 1 in current row
+//             int cnt = count(mat[i].begin(),mat[i].end(),1);
+//             // inserting cnt and index pair to set
+//             s.insert({cnt,i});
+//         }
+        
+//         vector<int> ans;
+//         // first k weakest row is our answer 
+//         for(auto i : s)
+//         {
+//             if(k == 0)
+//                 break;
+//             ans.push_back(i.second);
+//             --k;
+//         }
+        
+//         return ans;
+//     }
+    
+    
+//     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+
+//         return Weakest(mat,k);
+        
+//     }
+	
+// };
+
+// soldier(1) in each row = 2 4 1 2 5
+//indices of weakest rows = 0 1 2 3 4 
+//ans k= 3   [ 2, 0 , 3 ,1, 4]
+class Solution{
+ public:
+     typedef pair<int,int>P;
+      vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+          int m = mat.size();//rows
+          int n = mat[0].size(); //col
+          
+          vector<P> countOnes;//{1's count , row}
+          
+          for(int row = 0; row<m; row++){
+              int num_of_ones = count(mat[row].begin(),mat[row].end(),1);
+              countOnes.push_back({num_of_ones , row});
+          }
+          
+          sort(countOnes.begin() , countOnes.end());
+          
+          vector<int>ans;
+          for(auto i : countOnes){
+              if(k==0)
+                  break;
+              ans.push_back(i.second);
+              k--;
+          }
+          
+          return ans;
+      }
+};
