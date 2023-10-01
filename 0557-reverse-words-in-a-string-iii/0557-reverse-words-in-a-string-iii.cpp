06b1dff7ff1e2f264fa2caff954da7914a5db99d
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-10-01 02:15:10 -0400

    Time: 17 ms (46.37%), Space: 10 MB (59.58%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,6 +1,6 @@
-class Solution {
-public:
-    string reverseWords(string s) {
+// class Solution {
+// public:
+//     string reverseWords(string s) {
 //         int i = 0 , j=0;
 //         int n = s.size();
         
@@ -16,16 +16,45 @@ public:
 // };
 
 
-          int i = 0;
-        for(int j=0;j<s.length();j++)
-        {
-            if(s[j]==' ')
-            {
-                reverse(s.begin()+i,s.begin()+j);
-                i=j+1;
+//           int i = 0;
+//         for(int j=0;j<s.length();j++)
+//         {
+//             if(s[j]==' ')
+//             {
+//                 reverse(s.begin()+i,s.begin()+j);
+//                 i=j+1;
+//             }
+//         }
+//         reverse(s.begin()+i,s.end());
+//         return s;
+//     }
+// };
+
+class Solution {
+public:
+    string reverseWords(string s){
+        int n = s.length();
+        
+        int start = 0;
+        int end = 0;
+        
+        for(int i=0 ;i<=n ; i++){
+            //space or end of the word then reverse the word
+            if(s[i] == ' ' || i == n){
+                end = i-1;
+                //reverse word
+                while(start < end){
+                    swap(s[start] ,  s[end]);
+                    start++;
+                    end--;
+                    // take
+                    // i   j
+                    // e   t
+                    // e k a t
+                }
+                start = i+1;//for next word
             }
         }
-        reverse(s.begin()+i,s.end());
         return s;
     }
 };
