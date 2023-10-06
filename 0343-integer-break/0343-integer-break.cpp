4b3f8803ff9b2bd1382b0156888aca817e0c1d71
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-10-06 15:39:56 -0400

    Time: 0 ms (100.00%), Space: 6.5 MB (29.62%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,54 @@
+//  n =        5    *     *
+      // (1,4) (2,3) (3,2) (4,1) 
+         // |
+ // (1,3) (2,2) (3,1)
+
+// i*(n-i) or i*solve(n-i)
+//ans = i * max(n-i , solve(n-i))
+             
+//T.C= o(n * n^n-1)
+
+// class Solution {
+// public:
+//     int solve(int n){
+//         if(n == 1)
+//             return 1; // we can't break it further
+        
+//         int res = INT_MIN;
+        
+//         for(int i=1 ; i<=n-1 ; i++){
+//            int prod = i * max(n-i , solve(n-i));
+//            res = max(res , prod);
+//         }
+//         return res;
+//     }
+    
+//     int integerBreak(int n) {
+//         return solve(n);
+//     }
+// };
+
+//memo
+class Solution {
+public:
+    int t[59];
+    int solve(int n){
+        if(n == 1)
+            return 1; // we can't break it further
+        
+        if(t[n] != -1)
+            return t[n]; // alredy solved tree diagram stored 3 already(1,2)
+        int res = INT_MIN;
+        
+        for(int i=1 ; i<=n-1 ; i++){
+           int prod = i * max(n-i , solve(n-i));
+           res = max(res , prod);
+        }
+        return t[n] = res;
+    }
+    
+    int integerBreak(int n) {
+        memset(t, -1,sizeof(t));
+        return solve(n);
+    }
+};
