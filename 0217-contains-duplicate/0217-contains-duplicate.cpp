9219ab2fa86abd7a48dc19fc6400a20341276eee
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-10-21 02:54:55 -0400

    Time: 81 ms (95.82%), Space: 57.5 MB (86.73%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,26 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         int n = nums.size();
+        
+//         for(int i=0 ; i<n; i++){
+//             for(int j=i+1 ; j<n ;j++){
+//               if(nums[i] == nums[j])
+//                   return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        bool flag = false;
+        for(int i =0;i<nums.size()-1;i++){
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return flag;
+    }
+};
