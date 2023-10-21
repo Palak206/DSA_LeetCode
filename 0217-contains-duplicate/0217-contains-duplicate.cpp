3d8002fe71270492090b460fe826bf7a891d0977
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-10-21 03:07:46 -0400

    Time: 114 ms (28.66%), Space: 72 MB (13.59%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -13,13 +13,36 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+        
+//         for(int i =0;i<nums.size()-1;i++){
+//             if(nums[i] == nums[i+1]) return true;
+//         }
+//         return false;
+//     }
+// };
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         return nums.size() > set<int>(nums.begin(),nums.end()).size();
+//     }
+// };
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        unordered_map<int,int>mp;
         
-        for(int i =0;i<nums.size()-1;i++){
-            if(nums[i] == nums[i+1]) return true;
+        for(auto i : nums)
+            mp[i]++;
+        
+        for(auto i:mp){
+            if(i.second > 1)
+                return true;
         }
         return false;
     }
