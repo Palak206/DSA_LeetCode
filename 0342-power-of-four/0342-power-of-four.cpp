9f9ed9bf14eeb10299684feabb4b26e87ca8da46
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-10-23 14:09:32 -0400

    Time: 4 ms (22.26%), Space: 6.4 MB (13.31%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,15 +1,40 @@
+// // class Solution {
+// public:
+//     bool isPowerOfFour(int n) {
+//         if (n <= 0) {
+//             return false;
+//         }
+//         //16%4 4%4 then n= 1
+//         while (n % 4 == 0) {
+//             n /= 4;
+//         }
+//         //jese h n== 1 rh jaye that means it's a poer of 4
+//         if(n==1)
+//           return true;
+//         return false
+//     }
+// };
+
+//4^x == n take log both side
+//log(4^x) = logn
+// xlog4 = logn
+//x = logn/log4
+//if x is == n then true for ex
+ // n== 16 x = log16/log4 = 2
+//4^2 = 16 = n true
 class Solution {
 public:
     bool isPowerOfFour(int n) {
         if (n <= 0) {
             return false;
         }
-        while (n > 1) {
-            if (n % 4 != 0) {
-                return false;
-            }
-            n /= 4;
-        }
-        return true;
+        if(n == 1) return true;
+        
+        int x = log(n)/log(4);
+        //4^2 = 16 = n
+        if(pow(4,x) == n)
+            return true;
+        return false;
+        
     }
 };
