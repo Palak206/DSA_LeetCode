f4eb909b5249cc962275dd37161d70d5a7907ca1
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-10-26 16:00:39 -0400

    Time: 37 ms (60.14%), Space: 14.8 MB (60.24%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -7,82 +7,82 @@
 //  * };
 //  */
 
-// class Solution {
-// public:
-//     ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
-//         ListNode *p1 = head1;
-//         ListNode *p2 = head2;
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
+        ListNode *p1 = head1;
+        ListNode *p2 = head2;
 
-//         if(p1 == NULL || p2 == NULL)
-//           return NULL;
+        if(p1 == NULL || p2 == NULL)
+          return NULL;
 
-//         while( p1 != p2){
-//             // if(p1 == p2)
-//             //  return p1;
+        while( p1 != p2){
+            // if(p1 == p2)
+            //  return p1;
 
-//              if(p1 == NULL){
-//               p1 = head2;
-//             }
+             if(p1 == NULL){
+              p1 = head2;
+            }
 
-//             else{
-//                 p1 = p1->next;
-//             }
+            else{
+                p1 = p1->next;
+            }
 
-//             if(p2 == NULL){
-//               p2 = head1;
-//             }
+            if(p2 == NULL){
+              p2 = head1;
+            }
 
-//             else{
-//                 p2 = p2->next;
-//             }
+            else{
+                p2 = p2->next;
+            }
 
-//         }
+        }
 
-//         return p1;
-//     }
-// };
+        return p1;
+    }
+};
 
 //find the length of both the list 
 // Traverse the bigger linked list until the remaining nodes count becomes equal to the smaller one's.
 // Traverse both the heads together. If both of them are same then the intersecting node is found.
 
-class Solution {
-public:
-    int length(ListNode *head){
-        int len = 0;
-        while(head){
-            len++;
-            head = head->next;
-        }
-        return len;
-    }
+// class Solution {
+// public:
+//     int length(ListNode *head){
+//         int len = 0;
+//         while(head){
+//             len++;
+//             head = head->next;
+//         }
+//         return len;
+//     }
 
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
-        if(!headA || !headB)
-            return NULL;
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
+//         if(!headA || !headB)
+//             return NULL;
         
-        int len1 = length(headA);
-        int len2 = length(headB);
+//         int len1 = length(headA);
+//         int len2 = length(headB);
         
-        if(len1 > len2){
-            while(len1 > len2){
-                headA = headA->next;
-                len1--;
-            }
-        }
-        else if(len1 < len2){
-            while(len1 < len2){
-                headB = headB->next;
-                len2--;
-            }
-        }
+//         if(len1 > len2){
+//             while(len1 > len2){
+//                 headA = headA->next;
+//                 len1--;
+//             }
+//         }
+//         else if(len1 < len2){
+//             while(len1 < len2){
+//                 headB = headB->next;
+//                 len2--;
+//             }
+//         }
         
-        while(headA && headB){
-            if(headA == headB)
-                return headA;
-            headA = headA->next;
-            headB = headB->next;
-        }
-        return NULL;
-    }
-};
+//         while(headA && headB){
+//             if(headA == headB)
+//                 return headA;
+//             headA = headA->next;
+//             headB = headB->next;
+//         }
+//         return NULL;
+//     }
+// };
