b46d8f93debce37c0fbb5ed0e981e4cf603bd091
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-10-31 10:02:34 -0400

    Time: 0 ms (100.00%), Space: 11.2 MB (11.71%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -9,72 +9,82 @@
  * };
  */
 
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-
-        ListNode* fast = head;
-        ListNode* slow = head;
-
-        for(int i =1 ; i<=n ; i++){
-            fast = fast->next;
-        }
-        
-        if(fast==NULL){
-            return head->next;
-        }
-        
-        while(fast->next != NULL){
-            fast = fast->next;
-            slow = slow->next;
-        }
-
-        slow->next = slow->next->next;
-
-        return head;
-        
-
-
-    }
-};
-
 // class Solution {
-//     ListNode* reverse(ListNode* head){
-//         ListNode* curr = head , *temp;
-//         ListNode* prev = NULL;
-
-//         while(curr != NULL){
-//             temp = curr->next ;
-//             curr->next = prev;
-//             prev = curr;
-//             curr= temp;
-//         }
-//         return prev;
-//     }
 // public:
-//     ListNode* removeNthFromEnd(ListNode* head, int n){
-//         head = reverse(head);
-//         ListNode* curr = head  ;
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
 
-//     //    while(curr) curr = curr -> next;
-//         if(n==0){
-//          head = head->next;
-//          return head;
-//         }
+//         ListNode* fast = head;
+//         ListNode* slow = head;
 
-//         ListNode* prev = NULL;
-//         curr = head;
-//         for(int i = 0 ; i<=n-1 ; i++){
-//             prev = curr;
-//             curr = curr->next;
+//         for(int i =1 ; i<=n ; i++){
+//             fast = fast->next;
 //         }
         
-//         prev->next = curr->next;
+//         if(fast==NULL){
+//             return head->next;
+//         }
         
-//         return reverse(head);
+//         while(fast->next != NULL){
+//             fast = fast->next;
+//             slow = slow->next;
+//         }
+
+//         slow->next = slow->next->next;
+
+//         return head;
+        
+
 
 //     }
 // };
+
+// reverse the linkedlist 5 - 4 - 3 - 2 - 1
+// traverse until n = 0 
+class Solution {
+    ListNode* reverse(ListNode* head){
+        ListNode* curr = head , *temp;
+        ListNode* prev = NULL;
+
+        while(curr != NULL){
+            temp = curr->next ;
+            curr->next = prev;
+            prev = curr;
+            curr= temp;
+        }
+        return prev;
+    }
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n){
+        //create dummy node
+        ListNode* dummy =  new ListNode(0);
+        ListNode* curr = dummy;
+        curr->next = reverse(head);
+        // 0-5-4-3-2-1
+
+    //    while(curr) curr = curr -> next;
+        if(n==0){
+         head = head->next;
+         return head;
+        }
+
+        // ListNode* prev = NULL;
+        // curr = head;
+        int count=0;
+        ListNode* prev =NULL;
+        while(count<n && curr!=NULL)  //TRAVERSING UNTIL WE FIND THE REQUIRED NODE
+        {
+            prev=curr;
+            curr=curr->next;
+            count++;
+            
+        }
+        
+        prev->next=curr->next;        //REMOVING THE NODE
+        curr->next=NULL;
+        return reverse(dummy->next);     //REVERSING AGAIN (EXCLUDING THE DUMMY NODE)
+        // 1 2 3 5
+    }
+};
 
 // class Solution{
 // public:
