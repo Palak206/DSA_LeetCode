f8b3b78f25aea2bd2b12348122c028522e13e162
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-11-01 02:44:42 -0400

    Time: 12 ms (84.62%), Space: 24.2 MB (95.13%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -14,9 +14,50 @@
 
 //1st approach use hashmap to count the freq of element
 //if greater thn 1 then we stored in our ans
+//T.C & S.C = O(N)
+// class Solution {
+// public:
+//     unordered_map<int,int>mp;
+    
+//     //inorder trverasal
+//     void dfs(TreeNode* root){
+//         if(root == NULL)
+//             return;
+        
+//         dfs(root->left);
+//         mp[root->val]++;
+//         dfs(root->right);
+//     }
+    
+//     vector<int> findMode(TreeNode* root) {
+//         dfs(root);
+//         vector<int>res;
+//         int maxFreq = 0;
+        
+//         for(auto &m:mp){
+//             if(m.second > maxFreq){
+//                 //means we get  new freq
+//                 maxFreq = m.second;
+//                 res= {};//again initiliaze bcoz new elem found
+//                 //store in result
+//                 res.push_back(m.first);
+//              }
+//             //if currfreq == maxfreq then push in the answer
+//             else if(m.second == maxFreq){
+//                 res.push_back(m.first);
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+
 class Solution {
 public:
-    unordered_map<int,int>mp;
+    int currNum = 0;
+    int currFreq = 0;
+    int maxFreq = 0;
+    vector<int>ans;
     
     //inorder trverasal
     void dfs(TreeNode* root){
@@ -24,28 +65,32 @@ public:
             return;
         
         dfs(root->left);
-        mp[root->val]++;
+        
+        if(root->val == currNum){
+            currFreq++;
+        }
+        else{
+            currNum = root->val;
+            currFreq = 1;
+            //bcoz we got new ele
+        }
+        
+        //if currfreq > mxfreq then update the maxfreq in the ans
+        if(currFreq > maxFreq){
+            ans = {};
+            maxFreq = currFreq;
+        }
+        
+        if(currFreq == maxFreq){
+            ans.push_back(root->val);
+        }
+        
         dfs(root->right);
     }
     
     vector<int> findMode(TreeNode* root) {
         dfs(root);
-        vector<int>res;
-        int maxFreq = 0;
         
-        for(auto &m:mp){
-            if(m.second > maxFreq){
-                //means we get  new freq
-                maxFreq = m.second;
-                res= {};//again initiliaze bcoz new elem found
-                //store in result
-                res.push_back(m.first);
-             }
-            //if currfreq == maxfreq then push in the answer
-            else if(m.second == maxFreq){
-                res.push_back(m.first);
-            }
-        }
-        return res;
+        return ans;
     }
 };
