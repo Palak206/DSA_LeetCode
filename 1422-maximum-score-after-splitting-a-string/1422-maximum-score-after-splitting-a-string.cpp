f22bbf354427bc7f70642462d957a4df4534ff8a
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2023-12-22 14:12:04 -0500

    Time: 3 ms (50.89%), Space: 6.5 MB (87.50%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,27 @@
+//first we count the 1's in the string
+//then iterate from 0 to n-1
+//if s[i] == 1 then dec the 1's
+//if s[i] == 0 then zeroOnes++
+//res = max(res , zero+ones);
+
+class Solution {
+public:
+    int maxScore(string s) {
+        int n= s.length();
+        
+        int ones = count(s.begin() , s.end() , '1');
+        int zeros = 0;
+        int res = INT_MIN;
+        
+        for(int i=0 ; i<n-1 ; i++){
+            if(s[i] == '1'){
+                ones--;
+            }
+            else{
+                zeros++;
+            }
+            res = max(res , zeros+ones);
+        }
+        return res;
+    }
+};
