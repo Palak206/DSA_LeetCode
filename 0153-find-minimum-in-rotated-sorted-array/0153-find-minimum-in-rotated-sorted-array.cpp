9f920723cae7738aec870d80d5602e48d4e4ac01
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-01-06 00:29:07 -0500

    Time: 0 ms (100.00%), Space: 10.6 MB (33.07%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,20 +1,33 @@
-class Solution {
-public:
-    int findMin(vector<int>& nums) {
-        // int min_num;
-        // // sort(nums.begin() , nums.end());
-        return *min_element(nums.begin() , nums.end());
-        // return min_num;
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         return *min_element(nums.begin() , nums.end());
+//     }
+// };
+
+class Solution{
+    public:
+    int findMin(vector<int>& nums){
+        int n = nums.size();
+        int mini = INT_MAX;
         
+        for(int i=0; i<n; i++){
+            mini = min(mini,nums[i]);
+        }
+        return mini;
+    }
+};
+
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums){
         
 //         int n = nums.size();
 //         int low = 0 , high = n-1;
         
 //         while(low < high){
-//             // int mid = (low+high)/2;
 //             int mid = (low + high)/2;
-//           // int mid = low + (high - low)/2;
-            
+         
 //             if(nums[mid] > nums[high]){
 //                 low = mid+1;
 //             }
@@ -25,5 +38,5 @@ public:
 //         }
 //         return nums[low];
         
-    }
-};
+//     }
+// };
