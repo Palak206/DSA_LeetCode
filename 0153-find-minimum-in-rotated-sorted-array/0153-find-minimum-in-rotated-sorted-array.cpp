4169a5c6f71091a7d6674fe0aa453afd123b1d18
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-01-06 01:27:18 -0500

    Time: 3 ms (64.38%), Space: 10.4 MB (68.36%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -5,16 +5,44 @@
 //     }
 // };
 
+// class Solution{
+//     public:
+//     int findMin(vector<int>& nums){
+//         int n = nums.size();
+//         int mini = INT_MAX;
+        
+//         for(int i=0; i<n; i++){
+//             mini = min(mini,nums[i]);
+//         }
+//         return mini;
+//     }
+// };
+
+// If arr[low] <= arr[mid]: In this case, we identified that the left half is sorted.
+// If arr[mid] <= arr[high]: In this case, we identified that the right half is sorted.
+
 class Solution{
     public:
     int findMin(vector<int>& nums){
         int n = nums.size();
-        int mini = INT_MAX;
+        int l = 0, h = n-1;
+        int ans = INT_MAX;
         
-        for(int i=0; i<n; i++){
-            mini = min(mini,nums[i]);
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            
+            //if left half is sorted
+            if(nums[l] <= nums[mid]){
+                ans = min(ans , nums[l]);
+                l = mid+1;
+            }
+            else{
+                //if right half is sorted
+                ans = min(ans,nums[mid]);
+                h=mid-1;
+            }
         }
-        return mini;
+        return ans;
     }
 };
 
