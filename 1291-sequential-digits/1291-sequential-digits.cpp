7e23ce3a48a999654e53055530e354966f4f78ee
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-02-02 16:16:31 +0530

    Time: 3 ms (17.15%), Space: 7.4 MB (8.37%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -6,32 +6,67 @@
 //123 234 345 456 567 678 789
 
 //take 1%10 1+1=2 (1*10+2=12)
+
+//T.C & S.C = O(1)
+// class Solution {
+// public:
+//     vector<int> sequentialDigits(int low, int high) {
+//         queue<int>q;
+//         for(int i=1; i<=8; i++){
+//             q.push(i);
+//         }
+        
+//         vector<int>res;
+        
+//         while(!q.empty()){
+//             int temp = q.front();
+//             q.pop();
+            
+//             if(temp >= low && temp <= high){
+//                 //we found the answer
+//                 res.push_back(temp);
+//             }
+            
+//             int last_digit = temp%10;
+//             if(last_digit +1 <= 9){
+//                 q.push(temp*10+(last_digit + 1));
+//                 //1*10+(1+1) = 12
+//             }
+//         }
+//         return res;
+        
+//     }
+// };
+
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        queue<int>q;
-        for(int i=1; i<=8; i++){
-            q.push(i);
-        }
+        //find all possible digit that we already know
+        
+        vector<int> allPossible = {12,23,34,45,56,67,78,89,
+                                  123,234,345,456,567,678,789,
+                                  1234,2345,3456,4567,5678,6789,
+                                  12345,23456,34567,45678,56789,
+                                  123456,234567,345678,456789,
+                                  1234567,2345678,3456789,
+                                  12345678,23456789,
+                                  123456789};
         
         vector<int>res;
         
-        while(!q.empty()){
-            int temp = q.front();
-            q.pop();
-            
-            if(temp >= low && temp <= high){
-                //we found the answer
-                res.push_back(temp);
+        int n = allPossible.size();
+        
+        for(int i=0; i<n; i++){
+            if(allPossible[i] < low){
+                continue;
             }
             
-            int last_digit = temp%10;
-            if(last_digit +1 <= 9){
-                q.push(temp*10+(last_digit + 1));
-                //1*10+(1+1) = 12
+            if(allPossible[i] >high){
+                break;
             }
+            //if it's between low ad high then push in the res
+            res.push_back(allPossible[i]);
         }
         return res;
-        
     }
 };
