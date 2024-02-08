03d151b66e284b3d0a84b3cad005258986cf2245
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-02-09 01:15:21 +0530

    Time: 23 ms (71.15%), Space: 21.8 MB (74.57%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -8,24 +8,46 @@
  * };
  */
 
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if (root == NULL || root == p || root == q) {
+//             return root;
+//         }
+//         TreeNode* left = lowestCommonAncestor(root->left, p, q);
+//         TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+//         //result
+//         if(left == NULL) {
+//             return right;
+//         }
+//         else if(right == NULL) {
+//             return left;
+//         }
+//         else { //both left and right are not null, we found our result
+//             return root;
+//         }  
+        
+//     }
+// };
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == NULL || root == p || root == q) {
+        //if both the p and q is less than root vl then we can find our ans in root left
+        if(p->val < root->val && q->val < root->val){
+            return lowestCommonAncestor(root->left , p, q);
+        }
+        
+        //if both re greatr than root then go right
+        else if(p->val > root->val && q->val > root->val){
+            return lowestCommonAncestor(root->right, p , q);
+        }
+        
+        // if both not satisfy then return root the ans
+        else{
             return root;
         }
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-
-        //result
-        if(left == NULL) {
-            return right;
-        }
-        else if(right == NULL) {
-            return left;
-        }
-        else { //both left and right are not null, we found our result
-            return root;
-        }
+            
     }
 };
