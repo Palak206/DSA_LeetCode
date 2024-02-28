ff65cef781f4f430cd49145229697f7ee20cdf66
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-02-28 01:17:54 -0500

    Time: 7 ms (79.35%), Space: 20 MB (96.27%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -9,28 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
+// class Solution {
+// public:
     
-    int findBottomLeftValue(TreeNode* root) {
-        queue<TreeNode*> q;
-        TreeNode* curr = root;
+//     int findBottomLeftValue(TreeNode* root) {
+//         queue<TreeNode*> q;
+//         TreeNode* curr = root;
         
-        q.push(curr);
+//         q.push(curr);
         
-        while(!q.empty()){
-            curr = q.front(); //pop till we reach to the last level
-            q.pop();
+//         while(!q.empty()){
+//             curr = q.front(); //pop till we reach to the last level
+//             q.pop();
             
-            if(curr->right != NULL){
-                q.push(curr->right);
-            }
+//             if(curr->right != NULL){
+//                 q.push(curr->right);
+//             }
             
-            if(curr->left != NULL){
-                q.push(curr->left);
-            }
+//             if(curr->left != NULL){
+//                 q.push(curr->left);
+//             }
             
+//         }
+//         return curr->val;
+//     }
+// };
+
+class Solution {
+    int hmax = -1, ans = 0;
+    void dfs(TreeNode* root, int level){
+        if(!root) return;
+        if(level>hmax) {
+            hmax=level;
+            ans=root->val;
         }
-        return curr->val;
+        dfs(root->left, level+1);
+        dfs(root->right, level+1);
+    }
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        dfs(root,0);
+        return ans;
     }
 };
