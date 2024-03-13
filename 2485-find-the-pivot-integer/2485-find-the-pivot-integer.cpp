ccc11ca23722a5e35f87b39b7c5b4c1b19e15526
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-03-13 02:06:09 -0400

    Time: 0 ms (100.00%), Space: 7.2 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,18 +1,38 @@
+// class Solution {
+// public:
+//     int pivotInteger(int n) {
+//         int sum = 0;
+        
+//         for(int i = 1 ; i<=n ; i++){
+//             sum += i; // Initialize the sum of elements from 1 to x
+//             int temp = 0; // Initialize the sum of elements from x to n
+//             for(int j = i ; j<=n ; j++){
+//                 temp += j;
+//             }
+//             if(temp == sum)
+//                return i;
+            
+//         }
+//         return -1;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int pivotInteger(int n) {
-        int sum = 0;
+        int sum = n*(n+1)/2;
         
-        for(int i = 1 ; i<=n ; i++){
-            sum += i;
-            int temp = 0;
-            for(int j = i ; j<=n ; j++){
-                temp += j;
-            }
-            if(temp == sum)
-               return i;
-            
-        }
+        double a = sqrt(sum);
+        
+        if(a - ceil(a) == 0)
+            return int(a);
+        
         return -1;
     }
 };
+
+         
+
+        
