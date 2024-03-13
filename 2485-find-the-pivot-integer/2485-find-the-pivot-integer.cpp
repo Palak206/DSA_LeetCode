3247c2bca7ee12268aed173a28d6056032771ed4
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-03-13 01:54:58 -0400

    Time: 27 ms (12.53%), Space: 7.3 MB (15.68%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int sum = 0;
+        
+        for(int i = 1 ; i<=n ; i++){
+            sum += i;
+            int temp = 0;
+            for(int j = i ; j<=n ; j++){
+                temp += j;
+            }
+            if(temp == sum)
+               return i;
+            
+        }
+        return -1;
+    }
+};
