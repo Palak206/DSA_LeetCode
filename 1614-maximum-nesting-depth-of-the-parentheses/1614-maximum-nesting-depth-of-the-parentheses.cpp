b2868252d86589b36c8addc62c0dc2cbaf051071
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-04-04 06:13:17 -0400

    Time: 3 ms (40.00%), Space: 7.3 MB (78.96%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,19 +1,41 @@
+// class Solution {
+// public:
+//     int maxDepth(string s) {
+//         stack<char>st;
+//         int res = 0;
+        
+//         for(int i=0; i<s.size(); i++){
+//             if(s[i] == '('){
+//                 st.push(s[i]);
+//             }
+            
+//             else if(s[i] == ')'){
+//                 st.pop();
+//             }
+            
+//             res = max(res, (int)st.size());
+            
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
     int maxDepth(string s) {
-        stack<char>st;
+        int openBrackets = 0;
         int res = 0;
         
         for(int i=0; i<s.size(); i++){
             if(s[i] == '('){
-                st.push(s[i]);
+                openBrackets++;
             }
             
             else if(s[i] == ')'){
-                st.pop();
+                openBrackets--;
             }
             
-            res = max(res, (int)st.size());
+            res = max(res, openBrackets);
             
         }
         return res;
