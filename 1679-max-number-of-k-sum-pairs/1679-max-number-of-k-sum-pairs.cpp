a4c737b63f87d92ccfae7594c1e088ddfc4b4cf5
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-04-30 10:09:50 -0400

    Time: 133 ms (17.52%), Space: 76.5 MB (8.29%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -25,28 +25,56 @@
 // };
 
 
+// class Solution {
+// public:
+//     int maxOperations(vector<int>& nums, int k) {
+//         sort(nums.begin(),nums.end());
+        
+//         int n = nums.size();
+//         int i=0, j=n-1;
+        
+//         int count = 0;
+        
+//         while(i<j){
+//             if(nums[i]+nums[j] == k){
+//                 count++;
+//                 i++;
+//                 j--;
+//             }
+            
+//             else if(nums[i]+nums[j] > k){
+//                 j--;
+//             }
+//             else{
+//                 i++;
+//             }
+//         }
+        
+//         return count;
+//     }
+// };
+
+
+// For every element check if (K - nums[i]) is present in the map. If it is there then we got a pair ,and we decrease the frequency of (K - nums[i]) by 1 since we used one occurence of this element.
+// If we did not find (K - nums[i]) with frequency greater than zero, then we put our current element in the hashmap too.
+
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        
-        int n = nums.size();
-        int i=0, j=n-1;
-        
+        unordered_map<int,int>mp;
         int count = 0;
         
-        while(i<j){
-            if(nums[i]+nums[j] == k){
-                count++;
-                i++;
-                j--;
-            }
+        int n = nums.size();
+        
+        for(int i=0; i<n; i++){
+            int diff = k-nums[i];
             
-            else if(nums[i]+nums[j] > k){
-                j--;
+            if(mp[diff] > 0){
+                count++;
+                mp[diff]--;
             }
             else{
-                i++;
+                mp[nums[i]]++;
             }
         }
         
