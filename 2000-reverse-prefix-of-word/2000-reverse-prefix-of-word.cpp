67ec438d8a142cd6b10b53f07f8bf973849775d6
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-05-01 02:21:19 -0400

    Time: 3 ms (42.21%), Space: 7.5 MB (26.16%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,9 +1,33 @@
+// class Solution {
+// public:
+//     string reversePrefix(string word, char ch) {
+//         int chIdx = word.find(ch);
+//         reverse(word.begin(),word.begin() + chIdx + 1);
+        
+//         return word;
+//     }
+// };
+
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
         int chIdx = word.find(ch);
-        reverse(word.begin(),word.begin() + chIdx + 1);
         
-        return word;
+        if(chIdx == -1)
+            return word;// if ch not find in the string
+        
+        int n = word.length();
+        string res;
+        
+        for(int i=0; i<n; i++){
+            if(i <= chIdx){
+                res += word[chIdx-i]; //reverse
+            }
+            else{
+                res += word[i]; //last words
+            }
+        }
+        return res;
+    
     }
 };
