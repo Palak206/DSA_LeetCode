dcbed2ddb4c4d55979958b9981c9bff3ece9c49b
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-05-02 01:15:58 -0400

    Time: 19 ms (59.01%), Space: 22.9 MB (65.86%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,14 +1,38 @@
+// class Solution {
+// public:
+//     int findMaxK(vector<int>& nums) {        
+//         int maxi = -1;
+//         for(int i=0; i<nums.size(); i++){
+//             for(int j=i+1; j<nums.size(); j++){
+//                 if(nums[i] == (-nums[j])){
+//                     maxi = max(maxi , abs(nums[i]));
+//                 }
+//             }
+//         }
+//         return maxi;
+//     }
+// };
+
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {        
-        int maxi = -1;
-        for(int i=0; i<nums.size(); i++){
-            for(int j=i+1; j<nums.size(); j++){
-                if(nums[i] == (-nums[j])){
-                    maxi = max(maxi , abs(nums[i]));
-                }
+        sort(nums.begin(), nums.end());
+        
+        int i = 0;
+        int j =nums.size()-1;
+        
+        while(i<j){
+            if((-nums[i]) == nums[j]){
+                return abs(nums[j]);
+            }
+            
+            else if((-nums[i]) > nums[j]){
+                i++;
+            }
+            else{
+                j--;
             }
         }
-        return maxi;
+        return - 1;
     }
 };
