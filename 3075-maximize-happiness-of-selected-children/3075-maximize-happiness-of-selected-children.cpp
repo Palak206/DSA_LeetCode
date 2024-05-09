2046f07a96be94db5eacb8f674135cec50d5f724
PALAK GOYAL <94644931+Palak206@users.noreply.github.com>
2024-05-09 01:27:53 -0400

    Time: 204 ms (23.13%), Space: 116.7 MB (7.79%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,15 +1,41 @@
+// class Solution {
+// public:
+//     long long maximumHappinessSum(vector<int>& happiness, int k) {
+//         int n = happiness.size();
+//         sort(happiness.begin(), happiness.end(), greater<int>()); //5 4 3 2 1
+        
+//         int turns = 0;
+//         long long sum = 0;
+        
+//         for(int i=0; i<k; i++){
+//             sum += max(happiness[i]-turns, 0);
+            
+//             turns++;
+//         }
+//         return sum;
+//     }
+// };
+
+
+//priority queue
+
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         int n = happiness.size();
-        sort(happiness.begin(), happiness.end(), greater<int>()); //5 4 3 2 1
+        
+        priority_queue<int> pq; //max hep
+        
+        for(int i=0; i<n; i++){
+            pq.push(happiness[i]);
+        }
         
         int turns = 0;
         long long sum = 0;
         
         for(int i=0; i<k; i++){
-            sum += max(happiness[i]-turns, 0);
-            
+            sum += max(pq.top() - turns, 0);
+            pq.pop();
             turns++;
         }
         return sum;
